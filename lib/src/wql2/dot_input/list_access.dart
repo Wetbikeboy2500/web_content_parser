3c190d44418b3b49797c4c49f0cf9cb41455dc93
Matt <wetbikeboy2500@gmail.com>
2024-10-11 18:17:03 -0400

    Condense list ops

--- lib/src/wql2/dot_input/list_access.dart
+++ lib/src/wql2/dot_input/list_access.dart
@@ -15,28 +15,12 @@ class LastAccess extends ListAccess {
   dynamic process(dynamic input) => input.last;
 }
 
-class EvenAccess extends ListAccess {
-  const EvenAccess();
-  @override
-  List process(dynamic input) {
-    final List<dynamic> result = [];
-    for (var i = 0; i < input.length; i += 2) {
-      result.add(input[i]);
-    }
-    return result;
-  }
+class EvenAccess extends IndexRangeStepAccess {
+  const EvenAccess() : super(0, -1, 2);
 }
 
-class OddAccess extends ListAccess {
-  const OddAccess();
-  @override
-  List process(dynamic input) {
-    final List<dynamic> result = [];
-    for (var i = 1; i < input.length; i += 2) {
-      result.add(input[i]);
-    }
-    return result;
-  }
+class OddAccess extends IndexRangeStepAccess {
+  const OddAccess() : super(1, -1, 2);
 }
 
 int indexAccess(int index, int length) => index < 0 ? length + index : index;
@@ -52,24 +36,8 @@ class Index1Access extends ListAccess {
   }
 }
 
-class IndexRangeAccess extends ListAccess {
-  final int start;
-  final int end;
-  const IndexRangeAccess(this.start, this.end);
-  @override
-  List process(dynamic input) {
-    final int trueStart = indexAccess(start, input.length);
-    final int trueEnd = indexAccess(end, input.length);
-
-    final int min = trueStart < trueEnd ? trueStart : trueEnd;
-    final int max = trueStart < trueEnd ? trueEnd : trueStart;
-
-    final List<dynamic> result = [];
-    for (var i = min; i < max; i++) {
-      result.add(input[i]);
-    }
-    return result;
-  }
+class IndexRangeAccess extends IndexRangeStepAccess {
+  const IndexRangeAccess(int start, int end) : super(start, end, 1);
 }
 
 class IndexRangeStepAccess extends ListAccess {
